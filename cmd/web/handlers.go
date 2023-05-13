5036bf0f50601a33c2e0fdcbcc0ba053b5f6f07d
cagrikutayok <cagrikutayok@hotmail.com>
2023-05-13 13:07:17 +0300

    changes - migration

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"go-web/internal/models"
+	"go-web/internal/cards"
 	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
@@ -19,21 +22,45 @@ func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)
 	}
 
 	// read posted data
-
 	cardHolder := r.Form.Get("cardholder_name")
-	email := r.Form.Get("email")
+	email := r.Form.Get("cardholder_email")
 	paymentIntent := r.Form.Get("payment_intent")
 	paymentMethod := r.Form.Get("payment_method")
 	paymentAmount := r.Form.Get("payment_amount")
 	paymentCurrency := r.Form.Get("payment_currency")
 
+	card := cards.Card{
+		Secret: app.config.stripe.secret,
+		Key:    app.config.stripe.key,
+	}
+	pi, err := card.RetrievePaymentIntent(paymentIntent)
+
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	pm, err := card.GetPaymentMethod(paymentMethod)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	lastFour := pm.Card.Last4
+	expiryMonth := pm.Card.ExpMonth
+	expiryYear := pm.Card.ExpYear
+
 	data := make(map[string]interface{})
 	data["cardholder"] = cardHolder
+	data["email"] = email
 	data["pi"] = paymentIntent
 	data["pm"] = paymentMethod
 	data["pa"] = paymentAmount
 	data["pc"] = paymentCurrency
-	data["email"] = email
+	data["last_four"] = lastFour
+	data["expiry_month"] = expiryMonth
+	data["expiry_year"] = expiryYear
+	data["bank_return_code"] = pi.Charges.Data[0].ID
 
 	if err := app.renderTemplate(
 		w,
@@ -50,12 +77,13 @@ func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)
 // displays the page to buy one digit
 func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
 
-	widget := models.Widget{
-		ID:             1,
-		Name:           "Single Widget",
-		Description:    "A single widget",
-		Price:          1000,
-		InventoryLevel: 10,
+	id := chi.URLParam(r, "id")
+	widgetID, _ := strconv.Atoi(id)
+
+	widget, err := app.DB.GetWidget(widgetID)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
 	}
 
 	data := make(map[string]interface{})
